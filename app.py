82d89660550f00299fae64690a53dab34a4c07a0
Bassel Ashraf <baselashraff@Gmail.com>
2026-06-25 12:26:40 +0300

    edits

--- app.py
+++ app.py
@@ -15,8 +15,28 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("💰 Job Salary Prediction App")
-st.write("Predict employee salaries using machine learning.")
+st.title("Job Salary Prediction")
+
+st.write("Enter employee information to predict salary")
+
+st.sidebar.header("Employee Information")
+
+experience = st.number_input(
+    "Experience Years",
+    min_value=0,
+    max_value=50,
+    value=1
+)
+
+job_title = st.number_input("Job Title", 0, 20, 0)
+
+education_level = st.number_input("Education Level", 0, 10, 0)
+
+location = st.number_input("Location", 0, 20, 0)
+
+
+predict = st.button("Predict Salary")
+
 
 
 
@@ -69,7 +89,10 @@ label_encoder = LabelEncoder()
 for col in df.select_dtypes(include='object').columns:
     df[col] = label_encoder.fit_transform(df[col])
 
-X = df.drop('salary', axis=1)
+X = df[['experience_years',
+        'education_level',
+        'job_title',
+        'location']]
 y = df['salary']
 
 from sklearn.model_selection import train_test_split
@@ -87,16 +110,10 @@ linear.fit(X_train, y_train)
 
 ## one hot encoder
 
-X_train = pd.get_dummies(X_train, columns=['industry'], drop_first=True)
-X_test = pd.get_dummies(X_test, columns=['industry'], drop_first=True)
-
-print(X_train.dtypes)
-
 
 from sklearn.metrics import mean_squared_error, r2_score 
 from sklearn.metrics import accuracy_score,mean_absolute_error
 
-r2_score(y_test, y_pred)
 
 
 
@@ -104,11 +121,10 @@ r2_score(y_test, y_pred)
 X_train.head()
 
 
-X = df.drop('salary', axis=1)
-y = df['salary']
 
 
 X = pd.get_dummies(X, drop_first=True)
+
 
 from sklearn.model_selection import train_test_split
 
@@ -125,23 +141,28 @@ from sklearn.linear_model import LinearRegression
 
 linear.fit(X_train, y_train)
 
+if predict:
+
+    prediction = linear.predict([[
+        experience,
+        education_level,
+        job_title,
+        location
+    ]])[0]
+
+    st.write("Predicted Salary:")
+    st.success(f"${prediction:,.0f}")
+
 y_pred = linear.predict(X_test)
+
+r2_score(y_test, y_pred)
+
 
 from sklearn.metrics import r2_score
 
 print(r2_score(y_test, y_pred))
 
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-
-mae = mean_absolute_error(y_test, y_pred)
-mse = mean_squared_error(y_test, y_pred)
-rmse = mse ** 0.5
-r2 = r2_score(y_test, y_pred)
-
-print("MAE:", mae)
-print("MSE:", mse)
-print("RMSE:", rmse)
-print("R² Score:", r2)
 
 print(f"accuracy_score ={round(r2_score(y_test, y_pred)*100, 2),4}%100%")
 
